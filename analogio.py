03ff3f8df2480cf0d4cbb57a936578c3847c603e
Lennart Edlich <lennart.edlich@student.htw-berlin.de>
2023-12-15 20:38:08 +0100

    rapidTrigger fix

--- analogio.py
+++ analogio.py
@@ -1,6 +1,7 @@
 import analogio
 import digitalio
 import time
+import math
 
 import keypad
 
@@ -8,11 +9,38 @@ from kmk.scanners import Scanner
 from kmk.modules import Module
 from kmk.kmk_keyboard import KMKKeyboard
 from kmk.keys import KC
+from hallCali import input_range, output_range
 
 keyboard = KMKKeyboard()
 
+
+a=-16.1941
+b=0.81885
+c=1.66367
+d=-16.0382
+
+def remap_range(value, input_range, output_range):
+    input_min, input_max = input_range
+    output_min, output_max = output_range
+    
+    # Avoid division by zero
+    if input_max - input_min == 0:
+        return output_min
+    
+    # Perform the linear mapping
+    mv = (value - input_min) / (input_max - input_min) * (output_max - output_min) + output_min
+    
+    #a\left(1-e^{\left(-b\left(x_{1}+c\right)\right)}\right)-d
+    lut_val = a * (1 - math.exp(-b * (mv + c))) - d
+    
+    # Clip the result to make sure it falls within the output range
+    mapped_value = max(min(lut_val, output_max), output_min)
+    
+    return mapped_value
+
 class AnalogScanner(Scanner):
-    def __init__(self,inPins, outPins,numOfKeys,analogAttrib):
+    
+    def __init__(self,inPins,outPins,multiNum,numOfKeys,analogAttrib):
          
         self.inPins = [analogio.AnalogIn(pin) for pin in inPins] #setup pins where analog signals are read
         
@@ -20,14 +48,18 @@ class AnalogScanner(Scanner):
         for pin in self.outPins:
             pin.direction = digitalio.Direction.OUTPUT
         
-        self.aVal = [self.inPins[0].value]*16*len(inPins) #array to store current value
-        self.aValold = [self.inPins[0].value]*16*len(inPins) #array to store last value
-        self.que = [] #que to store keys which have to be pressed
+        self.multiNum = multiNum
         
-        self.pressed = [False]*16*len(inPins) #array to store state of keys 
+        self.aVal = [4]*self.multiNum*len(inPins) #array to store current value
+        self.aValold = [4]*self.multiNum*len(inPins) #array to store last value
+        self.que = [] #que to store keys which have to be pressed
+        #print(self.aVal)
+        self.pressed = [False]*self.multiNum*len(inPins) #array to store state of keys 
         self.numOfKeys = numOfKeys #number of keys
         
         self.analogAttrib = analogAttrib #per key info about threshold/press type
+        
+        
                
     @property
     def key_count(self):
@@ -39,42 +71,52 @@ class AnalogScanner(Scanner):
         or a KeyEvent object if the pin value exceeds the threshold.
         '''
         
-        if len(self.que) == 0: #if no key is in que
-            for i in range(16): #cycles through multiplexer
+        #print(self.offset)
+        if len(self.que) == 0: #if no key is in que 
+            for i in range(self.multiNum): #cycles through multiplexer
                 self.outPins[0].value = (i & 0b0001) != 0
                 self.outPins[1].value = (i & 0b0010) != 0
                 self.outPins[2].value = (i & 0b0100) != 0
                 self.outPins[3].value = (i & 0b1000) != 0
                 for index, j in enumerate(self.inPins):    #read and stored analog values
-                    self.aVal[i + 16 * index] = j.value
+                    self.aVal[i + self.multiNum * index] = j.value
             
+            for i in range(self.numOfKeys):
+                self.aVal[i] = remap_range(self.aVal[i], input_range[i+self.offset], output_range)
+                
             for i in range(self.numOfKeys): #checks if values have changed
-                #threshold mode
+                
+                #
                 if self.analogAttrib[keyboard.active_layers[0]][i + self.offset] > 0:
-                    if self.aVal[i] > self.analogAttrib[keyboard.active_layers[0]][i + self.offset] and self.pressed[i] == False:
+                    if self.aVal[i] < self.analogAttrib[keyboard.active_layers[0]][i + self.offset] and self.pressed[i] == False:
                         self.pressed[i] = True
                         self.que.append(i)
-                    elif self.aVal[i] < self.analogAttrib[keyboard.active_layers[0]][i + self.offset] and self.pressed[i] == True:
+                    elif self.aVal[i] > self.analogAttrib[keyboard.active_layers[0]][i + self.offset] and self.pressed[i] == True:
                         self.pressed[i] = False
                         self.que.append(i)
                 #rapid trigger mode      
                 elif self.analogAttrib[keyboard.active_layers[0]][i + self.offset] < 0: 
-                    if self.aVal[i] > self.aValold[i] - abs(self.analogAttrib[keyboard.active_layers[0]][i + self.offset]):
+                    if self.aVal[i] < self.aValold[i] - abs(self.analogAttrib[keyboard.active_layers[0]][i + self.offset]):
                         self.aValold[i] = self.aVal[i]
                         if not self.pressed[i]:
                             self.pressed[i] = True
                             self.que.append(i)
-                    elif self.aVal[i] < self.aValold[i] + abs(self.analogAttrib[keyboard.active_layers[0]][i + self.offset]):
+                    elif self.aVal[i] > self.aValold[i] + abs(self.analogAttrib[keyboard.active_layers[0]][i + self.offset]):
                         self.aValold[i] = self.aVal[i]
                         if self.pressed[i]:
                             self.pressed[i] = False
                             self.que.append(i)
-                     
-        elif len(self.que) > 0: #if keys in que: remove one from que and send return
+                    elif self.pressed[i] and self.aVal[i]>3.9: #edge case if key gets stuck
+                        self.aValold[i] = self.aVal[i]
+                        self.pressed[i] = False
+                        self.que.append(i)
+                    
+        elif len(self.que) > 0: #if keys in que: remove one from que and send return 
             temp = self.que.pop(0)
+            #print(temp, self.pressed[temp])
             return keypad.Event(temp+self.offset, self.pressed[temp])
 
-            
+        
         
         
     
